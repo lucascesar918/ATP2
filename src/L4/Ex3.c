f4c2b0897f1e240b855ea3e7225c8afeb4aaa9c4
Lucas César <52894434+lucascesar918@users.noreply.github.com>
2025-10-18 21:39:57 +0000

    add 3 to 6

---
+++ src/L4/Ex3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Argumentos insuficientes!\n\nUso: %s [arquivo]\n", argv[0]);
+        return 1;
+    }
+
+    FILE* fp = fopen(argv[1], "r");
+
+    if (!fp) {
+        perror("Falha ao abrir o arquivo.");
+        return 1;
+    }
+
+    char ch;
+    int count = 1;
+
+    while ((ch = fgetc(fp)) && ch != EOF)
+        if (ch == '\n')
+            count++;
+
+    printf("O arquivo %s tem %d linhas.\n", argv[1], count);
+
+    fclose(fp);
+
+    return 0;
+}
---
+++ src/L4/Ex4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        printf("Argumentos insuficientes!\n\nUso: %s [fonte] [destino]\n", argv[0]);
+        return 1;
+    }
+
+    FILE* src_fp = fopen(argv[1], "r");
+    FILE* dest_fp = fopen(argv[2], "w");
+
+    if (!src_fp || !dest_fp) {
+        perror("Erro ao abrir um dos arquivos.");
+        return 1;
+    }
+
+    char ch;
+
+    while((ch = fgetc(src_fp)) && ch != EOF)
+        fputc(ch, dest_fp);
+
+    fclose(src_fp);
+    fclose(dest_fp);
+
+    return 0;
+}
