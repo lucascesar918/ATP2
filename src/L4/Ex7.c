79ee90736e38cf8845c8b715d60c4a32b2bd4019
Lucas César <52894434+lucascesar918@users.noreply.github.com>
2025-10-22 20:51:45 +0000

    add 7 to 8

---
+++ src/L4/Ex7.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 5) {
+        fprintf(stderr, "Argumentos Insuficientes!\nUso: %s <string de busca> <string de substituição> <arquivo fonte> <arquivo de destino>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char* search_pattern = argv[1];
+    const char* replace_pattern = argv[2];
+    const char* src_filepath = argv[3];
+    const char* dest_filepath = argv[4];
+
+    FILE* file = fopen(src_filepath, "r");
+    FILE* dest_file = fopen(dest_filepath, "w+");
+
+    if (!file) {
+        perror("Erro ao abrir o arquivo");
+        return EXIT_FAILURE;
+    }
+
+    if (!dest_file) {
+        perror("Erro ao criar arquivo temporário");
+        return EXIT_FAILURE;
+    }
+
+    char ch;
+    int match;
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == search_pattern[0]) {
+            match = 1;
+            long current_position = ftell(file);
+
+            for (int i = 1; search_pattern[i] != '\0'; i++) {
+                if (fgetc(file) != search_pattern[i]) {
+                    match = 0;
+                    break;
+                }
+            }
+
+            if (match) {
+                fputs(replace_pattern, dest_file);
+                continue;
+            }
+            else {
+                fseek(file, current_position, SEEK_SET);
+                fputc(ch, file);
+            }
+        }
+        fputc(ch, dest_file);
+    }
+
+    fclose(file);
+    fclose(dest_file);
+    return 0;
+}
---
+++ src/L4/Ex8.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CHUNK_SIZE 16
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Argumentos insuficientes!\n\nUso: %s <arquivo fonte>\n", argv[0]);
+        return 1;
+    }
+
+    FILE* fd = fopen(argv[1], "rb");
+
+    if (!fd) {
+        perror("Falha ao ler arquivo!");
+        return 1;
+    }
+
+    char* buf = malloc(CHUNK_SIZE);
+
+    if (!buf) {
+        perror("Erro ao alocar memória para o buffer.");
+        return 1;
+    }
+
+    long long offset = 0;
+    size_t bytes_read;
+
+    while ((bytes_read = fread(buf, 1, CHUNK_SIZE, fd))) {
+        printf("%08llx: ", offset);
+        int i;
+        for (i = 0; i < CHUNK_SIZE; i++) {
+            if (i < bytes_read) {
+                printf("%02x ", (unsigned char) buf[i]);
+            }
+
+            if (i == 7)
+                printf("  ");
+        }
+        printf("    | ");
+        for (i = 0; i < CHUNK_SIZE; i++) {
+            if (i < bytes_read) {
+                if (buf[i] >= 32 && buf[i] <= 126)
+                    printf("%c", buf[i]);
+                else
+                    printf(". ");
+            }
+
+        }
+
+        printf("\n");
+
+        offset += bytes_read;
+    }
+
+    printf("\n\n");
+
+    free(buf);
+    fclose(fd);
+    return 0;
+}
