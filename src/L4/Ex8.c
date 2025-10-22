44a6dbe763f4503540c7434456db35af43125d80
lucascesar918 <lc.freitas@unesp.br>
2025-10-22 18:30:27 -0300

    minor fixes in 8

--- src/L4/Ex8.c
+++ src/L4/Ex8.c
@@ -3,59 +3,60 @@
 
 #define CHUNK_SIZE 16
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Argumentos insuficientes!\n\nUso: %s <arquivo fonte>\n", argv[0]);
-        return 1;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Argumentos insuficientes!\n\nUso: %s <arquivo fonte>\n",
+            argv[0]);
+    return 1;
+  }
+
+  FILE *fd = fopen(argv[1], "rb");
+
+  if (!fd) {
+    perror("Falha ao ler arquivo");
+    return 1;
+  }
+
+  char *buf = malloc(CHUNK_SIZE);
+
+  if (!buf) {
+    perror("Erro ao alocar memória para o buffer");
+    return 1;
+  }
+
+  long long offset = 0;
+  size_t bytes_read;
+
+  while ((bytes_read = fread(buf, 1, CHUNK_SIZE, fd))) {
+    printf("%016llx: ", offset);
+    int i;
+    for (i = 0; i < CHUNK_SIZE; i++) {
+      if (i < bytes_read)
+        printf("%02x ", (unsigned char)buf[i]);
+      else
+        printf("   ");
+
+      if (i == 7)
+        printf("  ");
+    }
+    printf("    | ");
+    for (i = 0; i < CHUNK_SIZE; i++) {
+      if (i < bytes_read) {
+        if (buf[i] >= 32 && buf[i] <= 126)
+          printf("%c", buf[i]);
+        else
+          putchar('.');
+      }
     }
 
-    FILE* fd = fopen(argv[1], "rb");
+    printf("\n");
 
-    if (!fd) {
-        perror("Falha ao ler arquivo!");
-        return 1;
-    }
+    offset += bytes_read;
+  }
 
-    char* buf = malloc(CHUNK_SIZE);
+  printf("\n\n");
 
-    if (!buf) {
-        perror("Erro ao alocar memória para o buffer.");
-        return 1;
-    }
-
-    long long offset = 0;
-    size_t bytes_read;
-
-    while ((bytes_read = fread(buf, 1, CHUNK_SIZE, fd))) {
-        printf("%08llx: ", offset);
-        int i;
-        for (i = 0; i < CHUNK_SIZE; i++) {
-            if (i < bytes_read) {
-                printf("%02x ", (unsigned char) buf[i]);
-            }
-
-            if (i == 7)
-                printf("  ");
-        }
-        printf("    | ");
-        for (i = 0; i < CHUNK_SIZE; i++) {
-            if (i < bytes_read) {
-                if (buf[i] >= 32 && buf[i] <= 126)
-                    printf("%c", buf[i]);
-                else
-                    printf(". ");
-            }
-
-        }
-
-        printf("\n");
-
-        offset += bytes_read;
-    }
-
-    printf("\n\n");
-
-    free(buf);
-    fclose(fd);
-    return 0;
+  free(buf);
+  fclose(fd);
+  return 0;
 }
